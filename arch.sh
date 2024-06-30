7da47c3eae4e90490ac711f782a02f9168345c14
Ukasz8 <puppeteermc@gmail.com>
2024-06-30 19:13:11 +0200

    dziala?

--- arch.sh
+++ arch.sh
@@ -18,9 +18,6 @@ sudo ufw enable
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
 
-#OH-MY-ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 #THEME
 echo 'N' | git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
@@ -28,6 +25,9 @@ echo 'N' | git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZS
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#OH-MY-ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 rm -rf ~/.p10k.zsh
 
